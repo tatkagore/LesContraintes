900121a097b126c004cf10f03d60300e0e1f40f1
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2024-02-12 15:52:05 +0100

    storyboard&buttons

--- LesContraintes/ViewController.swift
+++ LesContraintes/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    // Image and text data
+    var images = ["1", "2", "3", "4"]
+    var imageDescriptions = ["Fauteuil élégant accompagné de son repose pieds jaune orangé", "Ce luminaire est tout simplement parfait pour sublimer votre table a manger ou un salon", "Chaise blanche de style scandinave", " Sofa vert en velou"]
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateContent()
     }
+    
+    @IBAction func leftButtonPressed(_ sender: UIButton) {
+//            // Show the previous image and text
+            currentIndex = (currentIndex - 1 + images.count) % images.count
+            updateContent()
+        }
 
+        @IBAction func rightButtonPressed(_ sender: UIButton) {
+            // Show the next image and text
+            currentIndex = (currentIndex + 1) % images.count
+            updateContent()
+        }
 
+        func updateContent() {
+            imageView.image = UIImage(named: images[currentIndex])
+            textLabel.text = imageDescriptions[currentIndex]
+        }
 }
-
